8ba77cf5267f8b8606e7a1a3d7e1c60234d8d83a
Ahmed Abdelfattah <ama.elfattah@gmail.com>
2021-12-23 22:31:39 +0200

    Change to hexadecimals

--- Labs/Lab 7/Core/Src/main.c
+++ Labs/Lab 7/Core/Src/main.c
@@ -1,16 +1,9 @@
 #include "GPIO.h"
 
-void delay(int seconds)
-{
-    while (seconds--)
-    {
-    }
-}
-
 void ISR_LED(void)
 {
-    GPIOA_ODR ^= (1 << 1);
-    EXTI_PR |= (1 << 0);
+    GPIOA_ODR ^= (0x01 << 1);
+    EXTI_PR |= (0x01 << 0);
 }
 int main(void)
 {
@@ -18,11 +11,11 @@ int main(void)
     GPIO_EnableClock(0);
     GPIO_Init(0, 0, INPUT, PULL_UP);
     GPIO_Init(0, 1, OUTPUT, PUSH_PULL);
-    RCC_APB2ENR |= (1 << 14);
-    SYSCFG_EXTICR1 &= ~(1 << 0);
-    EXTI_IMR |= (1 << 0);
-    EXTI_FTSR |= (1 << 0);
-    NVIC_ISER |= (1 << 6);
+    RCC_APB2ENR |= (0x01 << 14);
+    SYSCFG_EXTICR1 &= ~(0x0f << 0);
+    EXTI_IMR |= (0x01 << 0);
+    EXTI_FTSR |= (0x01 << 0);
+    NVIC_ISER |= (0x01 << 6);
 
     while (1)
     {
