8ae4c2b074b622cfc57e5c5a00e47c20ae8bf23e
Ahmed Abdelfattah <ama.elfattah@gmail.com>
2021-12-24 21:09:12 +0200

    Add task 4

--- Labs/Lab 7/Core/Src/main.c
+++ Labs/Lab 7/Core/Src/main.c
@@ -12,7 +12,6 @@ int main(void)
     GPIO_Init(0, 0, INPUT, PULL_UP);
     GPIO_Init(0, 1, OUTPUT, PUSH_PULL);
     RCC_APB2ENR |= (0x01 << 14);
-    SYSCFG_EXTICR1 &= ~(0x0f << 0);
     EXTI_IMR |= (0x01 << 0);
     EXTI_FTSR |= (0x01 << 0);
     NVIC_ISER |= (0x01 << 6);
---
+++ Tasks/Task 4/Core/Src/main.c
@@ -0,0 +1,59 @@
+#include "GPIO.h"
+
+static unsigned char sevenSegHex[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66,
+                                        0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+unsigned char counter = 0;
+
+void ISR_Increment(void)
+{
+    counter = (counter == 9) ? 0 : counter + 1;
+
+    for (unsigned char i = 0; i < 7; i++)
+    {
+        GPIO_WritePin(1, i, (sevenSegHex[counter] & (1 << i)) >> i);
+    }
+
+    EXTI_PR |= (0x01 << 0);
+}
+
+void ISR_Decrement(void)
+{
+    counter = (counter == 0) ? 9 : counter - 1;
+
+    for (unsigned char i = 0; i < 7; i++)
+    {
+        GPIO_WritePin(1, i, (sevenSegHex[counter] & (1 << i)) >> i);
+    }
+
+    EXTI_PR |= (0x02 << 0);
+}
+
+int main(void)
+{
+
+    GPIO_EnableClock(0);
+    GPIO_EnableClock(1);
+    GPIO_Init(0, 0, INPUT, PULL_UP);
+    GPIO_Init(0, 1, INPUT, PULL_UP);
+
+    for (unsigned char i = 0; i < 7; i++)
+    {
+        GPIO_Init(1, i, OUTPUT, PUSH_PULL);
+    }
+
+    RCC_APB2ENR |= (0x01 << 14);
+    EXTI_IMR |= (0x03 << 0);
+    EXTI_FTSR |= (0x03 << 0);
+    NVIC_ISER |= (0x03 << 6);
+
+    for (unsigned char i = 0; i < 7; i++)
+    {
+        GPIO_WritePin(1, i, (sevenSegHex[counter] & (1 << i)) >> i);
+    }
+
+    while (1)
+    {
+    }
+
+    return 0;
+}
